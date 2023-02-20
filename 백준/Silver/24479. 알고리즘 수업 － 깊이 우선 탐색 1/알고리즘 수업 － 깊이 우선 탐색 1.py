b17fb24fc0a3548286ee4446084f9577a2cbcb27
slr-09 <101566590+slr-09@users.noreply.github.com>
2023-02-20 21:02:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 640 ms, Memory: 72100 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+count=1
+
+def dfs(graph, v, visited):
+  visited[v] = True
+  global count
+  turn[v] = count
+  count += 1
+  for i in graph[v]:
+    if not visited[i]:
+      dfs(graph, i, visited)
+
+n,m,r = map(int,input().split())
+graph=[[] for i in range(n+1)]
+visited=[False]*(n+1)
+turn = [0 for i in range(n+1)]
+for i in range(m):
+  u,v = map(int,input().split())
+  graph[u].append(v)
+  graph[v].append(u)
+
+for i in range(n):
+  graph[i+1].sort()
+
+dfs(graph, r, visited)
+
+for i in range(n):
+  print(turn[i+1])
