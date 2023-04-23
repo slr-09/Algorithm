00d96b4651f97bf3426bab07e8f88c346e883c13
GaEun Yu <101566590+slr-09@users.noreply.github.com>
2023-04-23 19:19:15 +0900

    Update 주유소.py

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,3 +1,4 @@
+# 17점
 import sys
 input = sys.stdin.readline
 
@@ -15,5 +16,39 @@ for i in range(n-1):
       sum += dist[j]
     answer += sum*cost[i]
     break
+
+print(answer)
+-----------------------------------------
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n=int(input())
+dist=deque(map(int,input().split()))
+cost=list(map(int,input().split()))
+cost.pop()
+answer=0
+for i in range(n-1):
+  if cost[i]>min(cost):
+    answer += cost[i]*dist.popleft()
+  elif cost[i]==min(cost):
+    answer += sum(dist)*cost[i]
+    break
+
+print(answer)
+
+-----------------------------------------
+# 100점
+import sys
+input = sys.stdin.readline
+
+n=int(input())
+dist=list(map(int,input().split()))
+cost=list(map(int,input().split()))
+answer=0
+mincost=cost[0]
+for i in range(n-1):
+  mincost = min(mincost,cost[i])
+  answer += mincost*dist[i]
 
 print(answer)
