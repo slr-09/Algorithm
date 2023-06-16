1a3b37fa978f5d0743bb39060687bd8027bde2de
GaEun Yu <101566590+slr-09@users.noreply.github.com>
2023-06-17 02:02:51 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 40 ms, Memory: 31416 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**5)
+
+n=int(input())
+d=[0]*1001
+d[1] = 1
+d[2] = 3
+
+def f(x):
+  if d[x] != 0:
+    return d[x]
+  d[x] = f(x-1)+2*f(x-2)
+  return d[x]
+  
+print(f(n)%10007)
