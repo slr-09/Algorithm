d928ede8fb4da27c96da30a13710d35dcd31e2d1
GaEun Yu <101566590+slr-09@users.noreply.github.com>
2023-06-28 20:55:30 +0900

    Update IOIOI.py

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,3 +1,27 @@
+[100점]
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+str = input().rstrip()
+
+i,cnt,answer = 0,0,0
+while i<=m-2:
+  if str[i:i+3] == "IOI":
+    i += 2
+    cnt += 1
+    if cnt == n:
+      cnt -= 1
+      answer += 1
+  else:
+    i += 1
+    cnt = 0 
+
+print(answer)
+
+-------------------------
+[50점]
 import sys
 input = sys.stdin.readline
 
@@ -17,5 +41,4 @@ for i in range(m):
   index = str.find(str2, i, m)
   if index!=-1 and index not in num:
     num.add(index)
-
 print(len(num))
