f1286c50b2a0b054bd8a25bc65dc7f20c7c9a495
GaEun Yu <101566590+slr-09@users.noreply.github.com>
2023-09-17 00:28:40 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = input().rstrip()
+if n=="NLCS":
+  print("North London Collegiate School")
+elif n=="BHA":
+  print("Branksome Hall Asia")
+elif n=="KIS":
+  print("Korea International School")
+elif n=="SJA":
+  print("St. Johnsbury Academy")
